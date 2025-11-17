3c170d4362e24c9213d57cdd2a8fdd9b91c856d9
Charles Haynes <haynescd@chop.edu>
2025-11-17 08:30:15 -0600

    Add catppuccin theme

--- nvim/.config/nvim/lua/haynescd/lazy/colors.lua
+++ nvim/.config/nvim/lua/haynescd/lazy/colors.lua
@@ -8,19 +8,36 @@ end
 
 -- lua/plugins/rose-pine.lua
 return {
+    --{
+    --    "rose-pine/neovim",
+    --    name = "rose-pine",
+    --    config = function()
+    --        require('rose-pine').setup({
+    --            variant = "moon",
+    --            dark_variant = "moon",
+    --            disable_background = true,
+    --            styles = {
+    --                italic = true,
+    --            },
+    --        })
+    --        ColorMyPencils();
+    --    end
+    --},
     {
-        "rose-pine/neovim",
-        name = "rose-pine",
+        "catppuccin/nvim",
+        name = "catppuccin",
         config = function()
-            require('rose-pine').setup({
-                variant = "moon",
-                dark_variant = "moon",
-                disable_background = true,
-                styles = {
-                    italic = true,
+            require('catppuccin').setup({
+                flavour = "macchiato",
+                transparent_background = true,
+                no_underline = true,
+                dim_inactive = {
+                    enabled = true, -- dims the background color of inactive window
+                    shade = "dark",
+                    percentage = 0.15, -- percentage of the shade to apply to the inactive window
                 },
             })
-            ColorMyPencils();
+            ColorMyPencils('catppuccin');
         end
     }
 
